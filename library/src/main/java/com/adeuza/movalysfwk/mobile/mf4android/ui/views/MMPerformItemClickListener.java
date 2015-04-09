4135825a4c034cb0e69d538bbf20981ebb0a5048
Laurent Michenaud <laurent.michenaud@soprasteria.com>
2015-04-09 09:13:46 +0000

    create trunk

--- library/src/main/java/com/adeuza/movalysfwk/mobile/mf4android/ui/views/MMPerformItemClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.adeuza.movalysfwk.mobile.mf4android.ui.views;
-
-import android.view.View;
-
-public interface MMPerformItemClickListener {
-	
-	public boolean onPerformItemClick( View p_oView, int p_iPosition, long p_iId, MMPerformItemClickView p_oListView );
-}
--- library/src/main/java/com/adeuza/movalysfwk/mobile/mf4android/ui/views/MMPerformItemClickView.java
+++
@@ -1,8 +0,0 @@
-package com.adeuza.movalysfwk.mobile.mf4android.ui.views;
-
-import android.view.View;
-
-public interface MMPerformItemClickView {
-
-	public boolean superPerformItemClick(View p_oView, int p_iPosition, long p_iId);
-}
