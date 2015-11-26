3a5aa5040b757019f3d3098af172ff2335bae32a
Laurent Bonzom <laurent.bonzom@soprasteria.com>
2015-11-26 15:13:43 +0000

    modif script .bat

--- updateMdk.bat
+++ updateMdk.bat
@@ -16,12 +16,10 @@ rmdir /s /q workdir
 mkdir workdir
 cd workdir
 
-REM Retrieve resources from apklib
-call mvn -B -q archetype:generate -DarchetypeGroupId=com.adeuza.movalysfwk -DarchetypeArtifactId=mdk-apklib-template -DarchetypeVersion=%MF4A_VERSION% -DgroupId=com.adeuza.movalysfwk -DartifactId=mdkapklib -Dversion=%MF4A_VERSION%
-
 xcopy /y "mdkapklib\mdk-ressources\res" "..\mdk\src\main\res\" /e
 
 REM Get mf4android
+svn export -q --force "%URL_REPO%/com/adeuza/movalysfwk/mdk-apklib-template/branches/Cotopaxi/src/main/resources/archetype-resources/mdk-ressources/res/" "../mdk/src/main/res/"
 svn export -q --force "%URL_REPO%/com/adeuza/movalysfwk/mobile/mf4android/%MF4A_SVNPATH%/core/src/main/java/" "../mdk/src/main/java"
 svn export -q --force "%URL_REPO%/com/adeuza/movalysfwk/mobile/mf4android/%MF4A_SVNPATH%/widget-legacy/src/main/java/" "../mdk-widget-legacy/src/main/java"
 svn export -q --force "%URL_REPO%/com/adeuza/movalysfwk/mobile/mf4android/%MF4A_SVNPATH%/database/src/main/java/" "../mdk-database/src/main/java"
