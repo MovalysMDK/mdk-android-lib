e7870ed10f011159e8bcb8bae91aebd50b329251
sbernardin <simon.bernardin@sopra.com>
2015-12-10 16:38:38 +0100

    Fix crash à la sortie par le menu d'une application générée

--- mdk/src/main/java/com/adeuza/movalysfwk/mobile/mf4android/activity/AbstractMMActivity.java
+++ mdk/src/main/java/com/adeuza/movalysfwk/mobile/mf4android/activity/AbstractMMActivity.java
@@ -244,7 +244,7 @@ public abstract class AbstractMMActivity extends LifecycleDispatchActionBarActiv
 	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu p_oMenu) {
-		if (!Application.getInstance().displayMandatorySettingWindow() || 
+		if (!Application.getInstance().displayMandatorySettingWindow() ||
 				!Application.getInstance().hasUndefinedMandatorySetting()) {
 			MenuInflater oInflater = this.getMenuInflater();
 
@@ -262,7 +262,7 @@ public abstract class AbstractMMActivity extends LifecycleDispatchActionBarActiv
 	 */
 	public List<Integer> getOptionMenuIds() {
 		List<Integer> r_listMenuIds = new ArrayList<>();
-		r_listMenuIds.add(this.androidApplication.getAndroidIdByRKey(AndroidApplicationR.menu_base));
+		r_listMenuIds.add(((AndroidApplication) Application.getInstance()).getAndroidIdByRKey(AndroidApplicationR.menu_base));
 		return r_listMenuIds ;
 	}
 
@@ -313,14 +313,14 @@ public abstract class AbstractMMActivity extends LifecycleDispatchActionBarActiv
 			oItem = p_oMenu.getItem(i);
 
 			try {
-				sTitle = this.getAndroidApplication().getStringResource(this.getAndroidApplication()
+				sTitle = Application.getInstance().getStringResource(((AndroidApplication)Application.getInstance())
 						.getAndroidIdStringByIntKey(oItem.getItemId()).concat("__title"));
 				if (sTitle != null) {
 					oItem.setTitle(sTitle);
 				}
 			} catch( Resources.NotFoundException oException ) { 
 				// No title override
-				this.getAndroidApplication().getLog().debug("AbstractMMActivity", "No title override");
+				Application.getInstance().getLog().debug("AbstractMMActivity", "No title override");
 			}
 
 			if (oItem.hasSubMenu()) {
